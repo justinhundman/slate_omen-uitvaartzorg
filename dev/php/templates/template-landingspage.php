fe8804059ebe3b318d9bcb889f142ac2cab4def0
Dylan Donkers <dylan@lokaalgevonden.nl>
2015-01-23 16:19:40 +0100

    push

--- dev/php/templates/template-landingspage.php
+++ dev/php/templates/template-landingspage.php
@@ -18,24 +18,27 @@ Template Name: landingspage
 		
 			<article class="u-gridRow" id="post-<?php the_ID(); ?>">
 					<article class="column u-gridCol4 column1" >
-						<a class="button" href="#">Persoonlijk</a>
+						<iframe class="iframe-home" width="560" height="315" src="//www.youtube.com/embed/k46wrXbWoXo" frameborder="0" allowfullscreen></iframe>
+						<a class="button" href="/persoonlijk/">Persoonlijk</a>
 					</article>
 					<article class="column u-gridCol4 column2">
-						<a class="button" href="#">Huys van herinnering</a>
+						<iframe class="iframe-home" width="560" height="315" src="//www.youtube.com/embed/dlEpZQsvUhM" frameborder="0" allowfullscreen></iframe>
+						<a class="button" href="/huys-van-herinnering/">Huys van Herinnering</a>
 					</article>
 					<article class="column u-gridCol4 column3">
-						<a class="button" href="#">De omarming</a>
+						<iframe class="iframe-home" width="560" height="315" src="//www.youtube.com/embed/-Mgi-PsSVFc" frameborder="0" allowfullscreen></iframe>
+						<a class="button" href="/de-omarming/">De Omarming</a>
 					</article>
 			</article>
 			<article class="u-gridRow" id="post-<?php the_ID(); ?>">
 					<article class="column u-gridCol4 column4">
-						<a class="button" href="#">Rouwdrukwerk</a>
+						<a class="button" href="/rouwdrukwerk-advertentie/">Rouwdrukwerk</a>
 					</article>
 					<article class="column u-gridCol4 column5">
-						<a class="button" href="#">Bloemen</a>
+						<a class="button" href="/bloemen/">Bloemen</a>
 					</article>
 					<article class="column u-gridCol4 column6">
-						<a class="button" href="#">Kisten en manden</a>
+						<a class="button" href="/uitvaartkisten-en-manden/">Kisten en manden</a>
 					</article>
 			</article>  
 			<article class="u-gridRow bottom-text" id="post-<?php the_ID(); ?>">
@@ -47,6 +50,8 @@ Template Name: landingspage
 					<p>Vanaf nu kunt u hier uw eigen uitvaartwensen digitaal ontwerpen en registreren. ‘Mijn Uitvaartwens’ kunt u opslaan in uw eigen beveiligde account. Omen Uitvaartzorg registreert uw wensen digitaal, zodat uw nabestaanden en de uitvaartverzorger er op het moment van overlijden onmiddellijk over kunnen beschikken. Het vastleggen van uw wensen op deze manier is niet rechtsgeldig en dient als leidraad voor de invulling van de uitvaart. Tevens is het een verzoek aan uw nabestaanden om uw uitvaartwensen te respecteren en daaraan tegemoet te komen. U kunt uw wensen ook vastleggen in een Uitvaartwensen brochure. Neem contact met ons op als u deze brochure wenst te ontvangen</p>
 				</article>
 			</article>
+
 		<?php endwhile; endif; ?> 
 	</div>
 <?php get_footer(); ?>  
+ 
